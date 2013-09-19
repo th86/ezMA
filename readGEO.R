6ddcc73c9a34e082d9e5bae726aad512bf36707e
th86 <th8623@gmail.com>
2013-09-19 12:41:54 -0400

    Function integrates reading GEO data, imputation, and log2 transformation

---
+++ readGEO.R
@@ -0,0 +1,42 @@
+#Read GEO series_matrix.txt files
+#Author: Tai-Hsien Ou Yang, Columbia University
+
+library("GEOquery")
+library("hgu133plus2.db")
+library(impute)
+
+
+#fileName="cytonormal.LAML.GSE12417-GPL570_series_matrix.txt"
+
+readGEO<-function( fileName ,saveRda=FALSE){
+
+	#Read from series_matrix.txt file
+	gse<-getGEO(filename= fileName)
+	e<-exprs(gse)
+	clinical<-phenoData(gse)
+	clinical<-clinical@data
+	#sample_id<-rownames(clinical@data)
+
+	#imputation
+	e<-impute.knn(e)$data
+	#Annotation
+	probeIDs<-ls(hgu133plus2SYMBOL)
+	symbols<-mget(probeIDs,hgu133plus2SYMBOL)
+	GPL570Symbols<-unlist(symbols)
+	genes.shared=intersect(rownames(e),names(GPL570Symbols))
+	e.reduced<-e[genes.shared,]
+	rownames(e.reduced)<- GPL570Symbols[genes.shared]
+
+	#log transformation
+	e.reduced=log2(e.reduced+0.5)
+
+	geo.data<-list()
+	geo.data$e<-e.reduced
+	geo.data$clinical<-clinical
+
+	if(saveRda==TRUE){
+		save(geo.data,  file=paste(fileName,".rda",sep="") )
+	}
+
+	return(geo.data)
+}
