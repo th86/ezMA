fbd9a53f11ffe72b9e5d8a04b8dd019eb137affe
th86 <th8623@gmail.com>
2013-09-25 15:06:58 -0400

    fix bugs in summary

--- bigGEO.R
+++ bigGEO.R
@@ -142,6 +142,8 @@ fastTabRead <- function(con,sep="\t",header=TRUE,sampleRows=100,
              dataTable = geoDataTable)
 }
 
+
+#by Tai-Hsien Ou Yang
 bigGEO <- function(fname,sampleSize=100) {
   gsmlist <- list()
   #gpllist <- list()
@@ -218,3 +220,58 @@ bigGEO <- function(fname,sampleSize=100) {
   #           gpls  = gpllist))
 }
 
+
+
+
+
+#Summarization by Tai-Hsien Ou Yang
+summarizeGEO<-function(){
+
+ptm <- proc.time()
+
+data.sources = list.files(pattern="*.rda")
+
+load("header.rda")
+sample_id<-header$sample_id
+
+#load( data.sources[1] ) #GPL platform data as the first
+#geneSymbol<-make.unique(as.character(Table(gsmlist[[ 1 ]]@dataTable)[,11]))
+
+load( data.sources[1] )
+probeset_ID<-Table(gsmlist[[2]]@dataTable)[,"ID_REF"]
+
+#if( "VALUE" %in% names(Table(gsmlist[[2]]@dataTable))==TRUE )
+#{
+  valName<- "VALUE"
+#}else{
+# valName<- "VALUE_DS"
+#}
+
+
+ge<-matrix(NA,length(probeset_ID), 1  )
+rownames(ge) <- probeset_ID
+
+
+
+for( file.itr in 1:(length(data.sources)-1) ){ #1 is GPL platform data
+  message(sprintf("Loading %d split...", file.itr ))
+
+  load( data.sources[file.itr] )
+  for( sample.itr in 1:length(gsmlist) ){
+    if( valName  %in% names(Table(gsmlist[[ sample.itr ]]@dataTable))){ 
+      ge.vec<-Table(gsmlist[[ sample.itr ]]@dataTable)[,valName]
+      ge=cbind(ge, ge.vec)
+      colnames(ge)[ncol(ge)]=names(gsmlist)[sample.itr]
+    }
+  }
+
+}
+ge=ge[,-1]
+
+
+save(ge,file="ge.rda")
+
+total.time =proc.time() - ptm
+cat("takes",total.time[1],"sec\n")
+
+}
